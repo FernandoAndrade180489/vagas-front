ee20acbece777afc7424e05b763bcf4e85875aa3
Fernando Andrade <fernandow.and@gmail.com>
2022-09-14 08:52:32 -0300

    Dynamic render with component :is

--- src/components/layouts/Conteudo.vue
+++ src/components/layouts/Conteudo.vue
@@ -1,9 +1,16 @@
 <template>
   <div>
-    <h1>{{titulo}}</h1>
+    <h1>{{ titulo }}</h1>
     <button @click="atualizarComponente()">Atualizar</button>
-    <home></home>
-    <publicar-vaga></publicar-vaga>
+
+    <!-- Renderizar de modo dinamico os componentes home e publicar-vaga. O nome tem que seguir o padrão do componente para permitir renderizar -->
+    <button @click="conteudo = 'home'">Home</button>
+    <button @click="conteudo = 'publicar-vaga'">Publicar Vaga</button>
+    <!-- 
+      <home></home>
+      <publicar-vaga></publicar-vaga> 
+    -->
+    <component :is="conteudo" />  
   </div>
 </template>
   
@@ -17,66 +24,66 @@ export default {
     PublicarVaga,
   },
   data: () => ({
-    teste: 'O componente foi criado',
-    titulo: 'Componente Conteudo',
+    teste: "O componente foi criado",
+    titulo: "Componente Conteudo",
+    conteudo: "home",
   }),
   methods: {
     atualizarComponente() {
-      this.titulo += '*';
-    }
+      this.titulo += "*";
+    },
   },
   beforeCreate() {
-    console.log('Antes de Criar - ', this.teste)
+    console.log("Antes de Criar - ", this.teste);
   },
   created() {
-    console.log('Criado - ', this.teste)
+    console.log("Criado - ", this.teste);
   },
   beforeMount() {
-    console.log('Antes de Montar o template - consumir dados de uma API')
+    console.log("Antes de Montar o template - consumir dados de uma API");
   },
   mounted() {
-    console.log('Montado - entrega o HTML no DOM Final')
+    console.log("Montado - entrega o HTML no DOM Final");
   },
 
   beforeUpdate() {
-    console.log('Antes de atualizar')
+    console.log("Antes de atualizar");
   },
   updated() {
-    console.log('Atualizado')
+    console.log("Atualizado");
   },
   beforeUnmount() {
-    console.log('Antes de desmontar/destruir')
+    console.log("Antes de desmontar/destruir");
   },
   unmounted() {
-    console.log('Desmontado/destruído')
+    console.log("Desmontado/destruído");
   },
 
   errorCaptured() {
-    console.log('Erro capturado')
+    console.log("Erro capturado");
   },
   renderTracked() {
-    console.log('Re-renderização rastreada')
+    console.log("Re-renderização rastreada");
   },
   renderTriggered() {
-    console.log('Re-renderização acionada')
+    console.log("Re-renderização acionada");
   },
   activated() {
-    console.log('Componente é ativado')
+    console.log("Componente é ativado");
   },
   deactivated() {
-    console.log('Componente desativado')
-  }
+    console.log("Componente desativado");
+  },
 };
 </script>
  
   
 <style module>
-
-  .teste-1 {
-    background-color: red;
-  }
-  #teste-2 {
-    background-color: yellow;
-  }
+.teste-1 {
+  background-color: red;
+}
+#teste-2 {
+  background-color: yellow;
+}
 </style>
   
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -12,8 +12,6 @@ export default {
    
     
 <style scoped>
-  h1 {
-    color: blue;
-  }
+ 
 </style>
     
