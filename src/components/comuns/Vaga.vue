4c88e89278a3dec19fe206cdef344f73b2809b93
Fernando Andrade <fernandow.and@gmail.com>
2022-09-14 10:40:00 -0300

    127. Tipagem de props

--- src/components/comuns/Vaga.vue
+++ src/components/comuns/Vaga.vue
@@ -16,7 +16,23 @@
 <script>
 export default {
   name: "Vaga",
-  props: ["titulo", "descricaoVaga", "salario", "modalidade", "tipo", "publicacao"],
+  // props: ["titulo", "descricaoVaga", "salario", "modalidade", "tipo", "publicacao"],
+  props: {
+    titulo: String,
+    descricaoVaga: String,
+    salario: [Number, String],
+    modalidade: String,
+    tipo: String,
+    publicacao: String,
+  },
+  created() {
+    console.log("titulo", typeof this.titulo);
+    console.log("descricaoVaga", typeof this.descricaoVaga);
+    console.log("salario", typeof this.salario);
+    console.log("modalidade", typeof this.modalidade);
+    console.log("tipo", typeof this.tipo);
+    console.log("publicacao", typeof this.publicacao);
+  },
 };
 </script>
 
